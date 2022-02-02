5a125c90b4dea80d96b18d05ab6c159d0a531b25
liord <lior.dikstein@altair-semi.com>
2022-02-02 15:33:18 +0200

    Updated readme file: added intallation instructions, added pytorch toturial link, added results for pytorch models. Added an example in toturials for using MCT with pytorch.

--- tutorials/example_pytorch_mobilenet_v2.py
+++ tutorials/example_pytorch_mobilenet_v2.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Sony Semiconductors Israel, Inc. All rights reserved.
+# Copyright 2022 Sony Semiconductors Israel, Inc. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,40 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
+from torchvision.models import mobilenet_v2
+from PIL import Image
+from torchvision import transforms
 import model_compression_toolkit as mct
-from tensorflow.keras.applications.mobilenet import MobileNet
 
 """
-This tutorial demonstrates how a model (more specifically, MobileNetV1) can be
+This tutorial demonstrates how a model (more specifically, MobileNetV2) can be
 quantized and optimized using the Model Compression Toolkit (MCT). 
 """
 
-####################################
-# Preprocessing images
-####################################
-import cv2
-import numpy as np
 
-MEAN = 127.5
-STD = 127.5
-RESIZE_SCALE = 256 / 224
-SIZE = 224
-
-
-def resize(x):
-    resize_side = max(RESIZE_SCALE * SIZE / x.shape[0], RESIZE_SCALE * SIZE / x.shape[1])
-    height_tag = int(np.round(resize_side * x.shape[0]))
-    width_tag = int(np.round(resize_side * x.shape[1]))
-    resized_img = cv2.resize(x, (width_tag, height_tag))
-    offset_height = int((height_tag - SIZE) / 2)
-    offset_width = int((width_tag - SIZE) / 2)
-    cropped_img = resized_img[offset_height:offset_height + SIZE, offset_width:offset_width + SIZE]
-    return cropped_img
-
-
-def normalization(x):
-    return (x - MEAN) / STD
+def np_to_pil(img):
+    return Image.fromarray(img)
 
 
 if __name__ == '__main__':
@@ -60,16 +39,24 @@ if __name__ == '__main__':
     # Create a representative data generator, which returns a list of images.
     # The images can be preprocessed using a list of preprocessing functions.
     from model_compression_toolkit import FolderImageLoader
-    image_data_loader = FolderImageLoader(folder,
-                                          preprocessing=[resize, normalization],
-                                          batch_size=batch_size)
 
+    image_data_loader = FolderImageLoader(folder,
+                                          preprocessing=[np_to_pil,
+                                                         transforms.Compose([
+                                                             transforms.Resize(256),
+                                                             transforms.CenterCrop(224),
+                                                             transforms.ToTensor(),
+                                                             transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                                                  std=[0.229, 0.224, 0.225]),
+                                                         ])
+                                                         ],
+                                          batch_size=batch_size)
 
     # Create a Callable representative dataset for calibration purposes.
     # The function should be called without any arguments, and should return a list numpy arrays (array for each
     # model's input).
-    # For example: A model has two input tensors - one with input shape of [32 X 32 X 3] and the second with
-    # an input shape of [224 X 224 X 3]. We calibrate the model using batches of 20 images.
+    # For example: A model has two input tensors - one with input shape of [3 X 32 X 32] and the second with
+    # an input shape of [3 X 224 X 224]. We calibrate the model using batches of 20 images.
     # Calling representative_data_gen() should return a list
     # of two numpy.ndarray objects where the arrays' shapes are [(20, 3, 32, 32), (20, 3, 224, 224)].
     def representative_data_gen() -> list:
@@ -77,7 +64,7 @@ if __name__ == '__main__':
 
     # Create a model and quantize it using the representative_data_gen as the calibration images.
     # Set the number of calibration iterations to 10.
-    model = MobileNet()
-    quantized_model, quantization_info = mct.keras_post_training_quantization(model,
+    model = mobilenet_v2(pretrained=True)
+    quantized_model, quantization_info = mct.pytorch_post_training_quantization(model,
                                                                               representative_data_gen,
                                                                               n_iter=10)
