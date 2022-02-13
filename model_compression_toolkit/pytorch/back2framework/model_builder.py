fix issues in pytorch mixed precision + unit tests

model_compression_toolkit/pytorch/back2framework/model_builder.py
@@ -142,13 +142,10 @@ class PytorchModelBuilder(torch.nn.Module):
         if mode == ModelBuilderMode.MIXEDPRECISION:
             for n in self.node_sort:
                 if not isinstance(n, FunctionalNode):
-                    if n.type is DummyPlaceHolder:
-                        # DummyPlaceHolder node always exists, and we just need to initiate it
-                        # but it's not relevant for MP
-                        self.add_module(n.name, node_builder(n))
-                    else:
+                    if n.is_weights_quantization_enabled():
                         self.add_module(n.name, PytorchMixedPrecisionWrapper(n, fw_info))
-                        # TODO: need to call set_model here as well? (called inside for the actual layer model)
+                    else:
+                        self.add_module(n.name, node_builder(n))
         else:
             for n in self.node_sort:
                 if not isinstance(n, FunctionalNode):

model_compression_toolkit/pytorch/mixed_precision/mixed_precision_wrapper.py
@@ -47,9 +47,14 @@ class PytorchMixedPrecisionWrapper(torch.nn.Module):
         self.layer.load_state_dict({k: torch.Tensor(v) for k, v in n.weights.items()}, strict=False)
         set_model(self.layer)
 
-        self.node_weights_q_cfg = n.candidates_weights_quantization_cfg
+        self.weight_attrs = fw_info.get_kernel_op_attributes(n.type)
+        # float_weights is a list of weights for each attribute that we want to quantize.
         self.float_weights = [n.get_weights_by_keys(attr) for attr in
-                              fw_info.get_kernel_op_attributes(n.type)]
+                              self.weight_attrs]
+
+        assert len(self.weight_attrs) == len(self.float_weights)
+
+        self.node_weights_q_cfg = n.candidates_weights_quantization_cfg
 
         self.quantizer_fn = self.node_weights_q_cfg[0].weights_quantization_fn
         for qc in self.node_weights_q_cfg:
@@ -71,19 +76,40 @@ class PytorchMixedPrecisionWrapper(torch.nn.Module):
     def _get_quantized_weights(self):
         quantized_weights = []
         for qc in self.node_weights_q_cfg:
-            quantized_weights.append(self.quantizer_fn(tensor_data=self.float_weights,
-                                                       n_bits=qc.weights_n_bits,
-                                                       signed=True,
-                                                       quantization_params=qc.weights_quantization_params,
-                                                       per_channel=qc.weights_per_channel_threshold,
-                                                       output_channels_axis=qc.weights_channels_axis))
+            # for each quantization configuration in mixed precision
+            # get quantized weights for each attribute and for each filter
+            quantized_per_attr = []
+            for float_weight in self.float_weights:
+                # for each attribute
+                quantized_per_attr.append(self.quantizer_fn(tensor_data=float_weight,
+                                                            n_bits=qc.weights_n_bits,
+                                                            signed=True,
+                                                            quantization_params=qc.weights_quantization_params,
+                                                            per_channel=qc.weights_per_channel_threshold,
+                                                            output_channels_axis=qc.weights_channels_axis))
+            quantized_weights.append(quantized_per_attr)
 
         return quantized_weights
 
-    def set_active_weights(self, bitwidth_idx):
+    def set_active_weights(self,
+                           bitwidth_idx: int,
+                           attr: str = None):
+        if attr is None:  # set bit width to all weights of the layer
+            attr_idxs = [attr_idx for attr_idx in range(len(self.quantized_weights[bitwidth_idx]))]
+            self._set_bit_width_index(bitwidth_idx, attr_idxs)
+        else:  # set bit width to a specific attribute
+            attr_idx = self.weight_attrs.index(attr)
+            self._set_bit_width_index(bitwidth_idx, [attr_idx])
+
+    def _set_bit_width_index(self, bitwidth_idx, attr_idxs):
         assert bitwidth_idx < len(self.quantized_weights), \
             f"Index {bitwidth_idx} does not exist in current quantization candidates list"
+
+        loaded_weights = {k: torch.as_tensor(v) for k, v in self.layer.state_dict().items()}
         with torch.no_grad():
-            # TODO: risky, need to verify that bitwidth_idx exist
-            weights_tensor = self.quantized_weights[bitwidth_idx]
-            self.layer.weight = torch.nn.Parameter(torch.from_numpy(weights_tensor).float())
+            for attr_idx in attr_idxs:
+                weights_tensor = self.quantized_weights[bitwidth_idx][attr_idx]
+                weights_device = loaded_weights[self.weight_attrs[attr_idx]].device
+                active_weights = torch.nn.Parameter(torch.from_numpy(weights_tensor).to(weights_device))
+                loaded_weights[self.weight_attrs[attr_idx]] = active_weights
+            self.layer.load_state_dict(loaded_weights, strict=True)

tests/pytorch_tests/model_tests/feature_models/mixed_precision_test.py
@@ -14,6 +14,7 @@
 # ==============================================================================
 import torch
 import numpy as np
+from torch.nn import Conv2d
 
 from model_compression_toolkit import MixedPrecisionQuantizationConfig, KPI
 from model_compression_toolkit.common.user_info import UserInformation
@@ -40,8 +41,7 @@ class MixedPercisionBaseTest(BasePytorchTest):
                                     relu_unbound_correction=False,
                                     input_scaling=False)
 
-        return {"mixed_precision_config":
-                    MixedPrecisionQuantizationConfig(qc, weights_n_bits=[2, 8, 4], num_of_images=1)}
+        return {"mixed_precision_model": MixedPrecisionQuantizationConfig(qc, weights_n_bits=[2, 8, 4], num_of_images=1)}
 
     # def get_input_shapes(self):
     #     return [[self.val_batch_size, 224, 244, 3]]
@@ -54,37 +54,59 @@ class MixedPercisionBaseTest(BasePytorchTest):
         # compare things to test.
         raise NotImplementedError
 
+    def compare_results(self, quantization_info, quantized_models, float_model, expected_bitwidth_idx):
+        # quantized with the highest precision since KPI==inf
+        self.unit_test.assertTrue((quantization_info.mixed_precision_cfg ==
+                                   [expected_bitwidth_idx, expected_bitwidth_idx]).all())
+        # verify that quantization occurred
+        quantized_model = quantized_models['mixed_precision_model']
+        conv_layers = list(filter(lambda _layer: type(_layer) == Conv2d, list(quantized_model.modules())))
+        float_conv_layers = list(filter(lambda _layer: type(_layer) == Conv2d, list(float_model.modules())))
+        for idx, layer in enumerate(conv_layers):  # quantized per channel
+            q_weights = layer.weight.detach().cpu().numpy()
+            float_weights = float_conv_layers[idx].weight.detach().cpu().numpy()
+            for i in range(3):
+                self.unit_test.assertTrue(
+                    np.unique(q_weights[i, :, :, :]).flatten().shape[0] <= 2 ** [8, 4, 2][expected_bitwidth_idx])
+            # quantized_model and float_model are not equal
+            self.unit_test.assertFalse((q_weights == float_weights).all())
 
-class MixedPercisionSearchBasic(MixedPercisionBaseTest):
+
+class MixedPercisionSearch8Bit(MixedPercisionBaseTest):
     def __init__(self, unit_test):
         super().__init__(unit_test)
 
     def get_kpi(self):
-        # kpi is for 2 bits on average
         return KPI(np.inf)
 
-    def compare(self, quantized_model, float_model, input_x=None, quantization_info=None):
-        assert (quantization_info.mixed_precision_cfg == [0, 0]).all()
-        # for i in range(30):  # quantized per channel
-        #     self.unit_test.assertTrue(
-        #         np.unique(quantized_model.layers[2].weights[0][:, :, :, i]).flatten().shape[0] <= 4)
-        # for i in range(50):  # quantized per channel
-        #     self.unit_test.assertTrue(
-        #         np.unique(quantized_model.layers[4].weights[0][:, :, :, i]).flatten().shape[0] <= 4)
+    def compare(self, quantized_models, float_model, input_x=None, quantization_info=None):
+        self.compare_results(quantization_info, quantized_models, float_model, 0)
+
+
+class MixedPercisionSearch2Bit(MixedPercisionBaseTest):
+    def __init__(self, unit_test):
+        super().__init__(unit_test)
+
+    def get_kpi(self):
+        return KPI(96)
+
+    def compare(self, quantized_models, float_model, input_x=None, quantization_info=None):
+        self.compare_results(quantization_info, quantized_models, float_model, 2)
 
 
 class MixedPrecisionNet(torch.nn.Module):
     def __init__(self, input_shape):
         super(MixedPrecisionNet, self).__init__()
         _, in_channels, _, _ = input_shape[0]
-        self.conv1 = torch.nn.Conv2d(in_channels, 4, kernel_size=3, stride=1)
-        # self.bn1 = torch.nn.BatchNorm2d()
-        # self.relu = torch.nn.ReLU()
+        self.conv1 = torch.nn.Conv2d(in_channels, 3, kernel_size=3)
+        self.bn1 = torch.nn.BatchNorm2d(3)
+        self.conv2 = torch.nn.Conv2d(3, 4, kernel_size=5)
+        self.relu = torch.nn.ReLU()
 
     def forward(self, inp):
         x = self.conv1(inp)
-        # x = self.bn1(x)
-        # x = self.conv2(x)
-        # output = self.relu(x)
-        return x
+        x = self.bn1(x)
+        x = self.conv2(x)
+        output = self.relu(x)
+        return output
 