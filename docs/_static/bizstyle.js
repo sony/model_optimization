6933db08b1a3bbffc13e3bc33dd186279dcb2f13
Reuven Peretz <reuvenp@altair-semi.com>
2022-03-27 17:12:32 +0300

    Update docs after updating version number (auto-generated)

--- docs/_static/bizstyle.js
+++ docs/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("MCT Documentation: ver 1.2.0");
+        $("li.nav-item-0 a").text("MCT Documentation: ver 1.3.0");
     }
 });
