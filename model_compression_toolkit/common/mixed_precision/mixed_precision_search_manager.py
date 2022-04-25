fix kpi matrix calculation and lp search test

model_compression_toolkit/common/mixed_precision/mixed_precision_search_manager.py
@@ -157,23 +157,20 @@ class MixedPrecisionSearchManager(object):
         configurable_sorted_nodes = self.graph.get_configurable_sorted_nodes()
         target_configurable_nodes = self.configurable_nodes_per_target[target]()
 
-        kpi_matrix = \
-            [
-                [
-                    [0 if c_n is not t_n else
-                     (self.compute_target_node_kpi(c, t, candidate_idx, target) -
-                      self.get_min_target_kpi(t, target))
-                     for candidate_idx in range(len(t_n.candidates_quantization_cfg))
-                     ]
-                    for c, c_n in enumerate(configurable_sorted_nodes)
-                ]
-                for t, t_n in enumerate(target_configurable_nodes)
-            ]
+        kpi_matrix = []
+        for t, t_n in enumerate(target_configurable_nodes):
+            configurable_nodes_kpis = []
+            for c, c_n in enumerate(configurable_sorted_nodes):
+                for candidate_idx in range(len(c_n.candidates_quantization_cfg)):
+                    configurable_nodes_kpis.append(self.candidate_kpi(t_n, t, c_n, c, candidate_idx, target))
+            kpi_matrix.append(np.asarray(configurable_nodes_kpis))
 
-        kpi_matrix = np.array(kpi_matrix)
+        return np.array(kpi_matrix)
 
-        # We need to reshape the returned matrix to a 2D array
-        return np.reshape(kpi_matrix, (kpi_matrix.shape[0], -1))
+    def candidate_kpi(self, target_node, target_idx, conf_node, conf_idx, candidate_idx, target):
+        return 0 if conf_node is not target_node else \
+            self.compute_target_node_kpi(conf_idx, target_idx, candidate_idx, target) - \
+            self.get_min_target_kpi(target_idx, target)
 
     def get_min_target_kpi(self, target_node_idx, target):
         """

tests/keras_tests/function_tests/test_lp_search_bitwidth.py
@@ -17,12 +17,14 @@ import unittest
 from functools import partial
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
 
+from model_compression_toolkit.common.constants import WEIGHTS, ACTIVATION
 from model_compression_toolkit.common.mixed_precision.distance_weighting import get_average_weights
 from model_compression_toolkit.common.mixed_precision.kpi import KPI
 from model_compression_toolkit.common.mixed_precision.mixed_precision_quantization_config import \
     MixedPrecisionQuantizationConfig
 from model_compression_toolkit.common.mixed_precision.mixed_precision_search_facade import search_bit_width, \
     BitWidthSearchMethod
+from model_compression_toolkit.common.mixed_precision.mixed_precision_search_manager import MixedPrecisionSearchManager
 from model_compression_toolkit.common.mixed_precision.search_methods.linear_programming import \
     mp_integer_programming_search
 from model_compression_toolkit.common.quantization.quantization_analyzer import analyzer_graph
@@ -39,6 +41,27 @@ from model_compression_toolkit.keras.keras_implementation import KerasImplementa
 from tests.common_tests.helpers.generate_test_hw_model import generate_test_hw_model
 
 
+class MockMixedPrecisionSearchManager:
+    def __init__(self, layer_to_kpi_mapping):
+        self.layer_to_bitwidth_mapping = {0: [0, 1, 2]}
+        self.layer_to_kpi_mapping = layer_to_kpi_mapping
+        self.compute_metric_fn = lambda x, y: 0
+        self.min_kpi = {WEIGHTS: [[1], [1], [1]], ACTIVATION: [[1], [1], [1]]}  # minimal kpi in the tests layer_to_kpi_mapping
+        self.kpi_aggr_methods = {WEIGHTS: lambda v: [sum(v)], ACTIVATION: lambda v: [sum(v)]}
+
+    def compute_kpi_metrix(self, target):
+        # minus 1 is normalization by the minimal kpi (which is always 1 in this test)
+        if target == WEIGHTS:
+            kpi_matrix = [np.flip(np.array([kpi.weights_memory - 1 for _, kpi in self.layer_to_kpi_mapping[0].items()]))]
+        elif target == ACTIVATION:
+            kpi_matrix = [np.flip(np.array([kpi.activation_memory - 1 for _, kpi in self.layer_to_kpi_mapping[0].items()]))]
+        else:
+            # not supposed to get here
+            kpi_matrix = []
+
+        return np.array(kpi_matrix)
+
+
 class TestLpSearchBitwidth(unittest.TestCase):
 
     def test_search_weights_only(self):
@@ -46,13 +69,9 @@ class TestLpSearchBitwidth(unittest.TestCase):
         layer_to_kpi_mapping = {0: {2: KPI(weights_memory=1),
                                     1: KPI(weights_memory=2),
                                     0: KPI(weights_memory=3)}}
+        mock_search_manager = MockMixedPrecisionSearchManager(layer_to_kpi_mapping)
 
-        bit_cfg = mp_integer_programming_search({0: [0, 1, 2]},
-                                                lambda x, y: 0,
-                                                lambda x, compute_weights_kpi=True, compute_activation_kpi=True:
-                                                layer_to_kpi_mapping[0][x[0]],
-                                                min_weights_cfg=[2],
-                                                min_activation_cfg=[2],
+        bit_cfg = mp_integer_programming_search(mock_search_manager,
                                                 target_kpi=target_kpi)
 
         self.assertTrue(len(bit_cfg) == 1)
@@ -60,20 +79,10 @@ class TestLpSearchBitwidth(unittest.TestCase):
 
         target_kpi = KPI(weights_memory=0)  # Infeasible solution!
         with self.assertRaises(Exception):
-            bit_cfg = mp_integer_programming_search({0: [0, 1, 2]},
-                                                    lambda x, y: 0,
-                                                    lambda x, compute_weights_kpi=True, compute_activation_kpi=True:
-                                                    layer_to_kpi_mapping[0][x[0]],
-                                                    min_weights_cfg=[2],
-                                                    min_activation_cfg=[2],
+            bit_cfg = mp_integer_programming_search(mock_search_manager,
                                                     target_kpi=target_kpi)
 
-        bit_cfg = mp_integer_programming_search({0: [0, 1, 2]},
-                                                lambda x, y: 0,
-                                                lambda x, compute_weights_kpi=True, compute_activation_kpi=True:
-                                                layer_to_kpi_mapping[0][x[0]],
-                                                min_weights_cfg=[2],
-                                                min_activation_cfg=[2],
+        bit_cfg = mp_integer_programming_search(mock_search_manager,
                                                 target_kpi=KPI(weights_memory=np.inf))
 
         self.assertTrue(len(bit_cfg) == 1)
@@ -84,13 +93,9 @@ class TestLpSearchBitwidth(unittest.TestCase):
         layer_to_kpi_mapping = {0: {2: KPI(activation_memory=1),
                                     1: KPI(activation_memory=2),
                                     0: KPI(activation_memory=3)}}
+        mock_search_manager = MockMixedPrecisionSearchManager(layer_to_kpi_mapping)
 
-        bit_cfg = mp_integer_programming_search({0: [0, 1, 2]},
-                                                lambda x, y: 0,
-                                                lambda x, compute_weights_kpi=True, compute_activation_kpi=True:
-                                                layer_to_kpi_mapping[0][x[0]],
-                                                min_weights_cfg=[2],
-                                                min_activation_cfg=[2],
+        bit_cfg = mp_integer_programming_search(mock_search_manager,
                                                 target_kpi=target_kpi)
 
         self.assertTrue(len(bit_cfg) == 1)
@@ -98,20 +103,10 @@ class TestLpSearchBitwidth(unittest.TestCase):
 
         target_kpi = KPI(activation_memory=0)  # Infeasible solution!
         with self.assertRaises(Exception):
-            bit_cfg = mp_integer_programming_search({0: [0, 1, 2]},
-                                                    lambda x, y: 0,
-                                                    lambda x, compute_weights_kpi=True, compute_activation_kpi=True:
-                                                    layer_to_kpi_mapping[0][x[0]],
-                                                    min_weights_cfg=[2],
-                                                    min_activation_cfg=[2],
+            bit_cfg = mp_integer_programming_search(mock_search_manager,
                                                     target_kpi=target_kpi)
 
-        bit_cfg = mp_integer_programming_search({0: [0, 1, 2]},
-                                                lambda x, y: 0,
-                                                lambda x, compute_weights_kpi=True, compute_activation_kpi=True:
-                                                layer_to_kpi_mapping[0][x[0]],
-                                                min_weights_cfg=[2],
-                                                min_activation_cfg=[2],
+        bit_cfg = mp_integer_programming_search(mock_search_manager,
                                                 target_kpi=KPI(activation_memory=np.inf))
 
         self.assertTrue(len(bit_cfg) == 1)
@@ -122,13 +117,9 @@ class TestLpSearchBitwidth(unittest.TestCase):
         layer_to_kpi_mapping = {0: {2: KPI(weights_memory=1, activation_memory=1),
                                     1: KPI(weights_memory=2, activation_memory=2),
                                     0: KPI(weights_memory=3, activation_memory=3)}}
+        mock_search_manager = MockMixedPrecisionSearchManager(layer_to_kpi_mapping)
 
-        bit_cfg = mp_integer_programming_search({0: [0, 1, 2]},
-                                                lambda x, y: 0,
-                                                lambda x, compute_weights_kpi=True, compute_activation_kpi=True:
-                                                layer_to_kpi_mapping[0][x[0]],
-                                                min_weights_cfg=[2],
-                                                min_activation_cfg=[2],
+        bit_cfg = mp_integer_programming_search(mock_search_manager,
                                                 target_kpi=target_kpi)
 
         self.assertTrue(len(bit_cfg) == 1)
@@ -136,20 +127,10 @@ class TestLpSearchBitwidth(unittest.TestCase):
 
         target_kpi = KPI(weights_memory=0, activation_memory=0)  # Infeasible solution!
         with self.assertRaises(Exception):
-            bit_cfg = mp_integer_programming_search({0: [0, 1, 2]},
-                                                    lambda x, y: 0,
-                                                    lambda x, compute_weights_kpi=True, compute_activation_kpi=True:
-                                                    layer_to_kpi_mapping[0][x[0]],
-                                                    min_weights_cfg=[2],
-                                                    min_activation_cfg=[2],
+            bit_cfg = mp_integer_programming_search(mock_search_manager,
                                                     target_kpi=target_kpi)
 
-        bit_cfg = mp_integer_programming_search({0: [0, 1, 2]},
-                                                lambda x, y: 0,
-                                                lambda x, compute_weights_kpi=True, compute_activation_kpi=True:
-                                                layer_to_kpi_mapping[0][x[0]],
-                                                min_weights_cfg=[2],
-                                                min_activation_cfg=[2],
+        bit_cfg = mp_integer_programming_search(mock_search_manager,
                                                 target_kpi=KPI(weights_memory=np.inf, activation_memory=np.inf))
 
         self.assertTrue(len(bit_cfg) == 1)