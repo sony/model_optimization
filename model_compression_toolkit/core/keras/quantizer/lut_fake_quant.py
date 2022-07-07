09eea85bf494b928819ea5241d8e2ffb87b82f1e
Ofir Gordon <Ofir.Gordon@altair-semi.com>
2022-07-07 11:36:07 +0300

    Minor fix in fake quant layer methods and type hints

--- model_compression_toolkit/core/keras/quantizer/lut_fake_quant.py
+++ model_compression_toolkit/core/keras/quantizer/lut_fake_quant.py
@@ -10,7 +10,7 @@ from model_compression_toolkit.core.common.constants import SIGNED, CLUSTER_CENT
 
 
 def activation_lut_kmean_quantizer(activation_n_bits: int,
-                                   quantization_params: dict) -> Callable:
+                                   quantization_params: Dict[str, np.ndarray]) -> Callable:
     """
     Builds a LUT quantizer for layer's activation using the provided params (threshold and clusters).
     It initiates a fake custom LUT layer that provides the quantizer function.
@@ -49,7 +49,7 @@ class LUTFakeQuant(Layer):
         """
         super(LUTFakeQuant, self).build(input_shape)
 
-    def call(self, input_data: Any, **kwargs) -> TFReference:
+    def call(self, input_data: TFReference, **kwargs) -> TFReference:
         """
 
         Args:
@@ -65,7 +65,7 @@ class LUTFakeQuant(Layer):
         _quant_output = self.lut_kmeans_quantizer(input_data)
         return _quant_output
 
-    def lut_kmeans_quantizer(self, tensor_data: Any) -> TFReference:
+    def lut_kmeans_quantizer(self, tensor_data: TFReference) -> TFReference:
         """
         Quantize a tensor using a non-uniform quantization based on the pre-defined kmeans clusters.
         1. Scales tensor_data with the threshold into 8-bit quantization range.
@@ -79,7 +79,7 @@ class LUTFakeQuant(Layer):
         Returns: Quantized tensor.
         """
 
-        tensor = self.int_quantization_with_threshold(tensor_data, self.threshold, MULTIPLIER_N_BITS)
+        tensor = self.int_quantization_with_threshold(tensor_data, MULTIPLIER_N_BITS)
         tensor = tf.expand_dims(tensor, -1)
 
         expanded_cluster_centers = self.cluster_centers.reshape([*[1 for _ in range(len(tensor.shape)-1)], -1])
@@ -91,16 +91,14 @@ class LUTFakeQuant(Layer):
         return quant_tensor
 
     def int_quantization_with_threshold(self,
-                                        data: Any,
-                                        threshold: np.ndarray,
+                                        data: TFReference,
                                         n_bits: int,
-                                        eps: float = EPS) -> Any:
+                                        eps: float = EPS) -> TFReference:
         """
         Divides data by threshold and quantize it to integers in the quantization range (depends on signed value).
 
         Args:
             data: tensor data.
-            threshold: threshold to divide the data.
             n_bits: number of bits that determines the quantization range.
             eps: Small value for numerical stability in division.
 
@@ -116,5 +114,5 @@ class LUTFakeQuant(Layer):
             clip_max = 2 ** n_bits - 1
             clip_min = 0
 
-        return tf.clip_by_value((data / (threshold + eps)) * (2 ** (n_bits - int(self.activation_is_signed))),
+        return tf.clip_by_value((data / (self.threshold + eps)) * (2 ** (n_bits - int(self.activation_is_signed))),
                                 clip_value_max=clip_max, clip_value_min=clip_min)
