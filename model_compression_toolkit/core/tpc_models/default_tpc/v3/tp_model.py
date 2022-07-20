01ad8369865279fda397248378159b01e1c0fa57
eladco <elad.cohen2@altair-semi.com>
2022-07-20 15:16:40 +0300

    comment out conv_add_relu, conv_relu_add fusions

--- model_compression_toolkit/core/tpc_models/default_tpc/v3/tp_model.py
+++ model_compression_toolkit/core/tpc_models/default_tpc/v3/tp_model.py
@@ -151,8 +151,8 @@ def generate_tp_model(default_config: OpQuantizationConfig,
         # ------------------- #
         tp.Fusing([conv, activations_after_conv_to_fuse])
         tp.Fusing([fc, activations_after_fc_to_fuse])
-        tp.Fusing([conv, add, any_relu])
-        tp.Fusing([conv, any_relu, add])
         tp.Fusing([any_binary, any_relu])
+        # tp.Fusing([conv, add, any_relu])
+        # tp.Fusing([conv, any_relu, add])
 
     return generated_tpc
