b93da027f1a3264d9dab75d8ef7a3e011f2ce9cf
Elad Cohen <78862769+elad-c@users.noreply.github.com>
2022-08-10 10:11:29 +0300

    fix license (#332)
    Co-authored-by: elad cohen <elad.cohen@altair-semi.com>

--- tests/keras_tests/feature_networks_tests/feature_networks/multiple_inputs_node_tests.py
+++ tests/keras_tests/feature_networks_tests/feature_networks/multiple_inputs_node_tests.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Sony Semiconductors Israel, Inc. All rights reserved.
+# Copyright 2021 Sony Semiconductor Israel, Inc. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
