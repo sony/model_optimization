ebefd414f00e2cd0193ce264d22a0f549a411192
eladc-git <eladcoh3@gmail.com>
2022-08-16 13:52:56 +0300

    Add common implementation to common gptq_training

--- model_compression_toolkit/gptq/common/gptq_training.py
+++ model_compression_toolkit/gptq/common/gptq_training.py
@@ -13,9 +13,10 @@
 # limitations under the License.
 # ==============================================================================
 from abc import ABC, abstractmethod
+import numpy as np
 from typing import Callable
 from model_compression_toolkit.gptq.common.gptq_config import GradientPTQConfig
-from model_compression_toolkit.core.common import Graph
+from model_compression_toolkit.core.common import Graph, Logger
 from model_compression_toolkit.core.common.framework_info import FrameworkInfo
 from model_compression_toolkit.core.common.framework_implementation import FrameworkImplementation
 from model_compression_toolkit.gptq.common.gptq_graph import get_compare_points
@@ -26,6 +27,7 @@ class GPTQTrainer(ABC):
     """
     Abstract GPTQ training class for fine-tuning a quantized model
     """
+
     def __init__(self,
                  graph_float: Graph,
                  graph_quant: Graph,
@@ -62,6 +64,104 @@ class GPTQTrainer(ABC):
                                                                        fw_info=self.fw_info)
 
         self.fxp_model, self.gptq_user_info = self.build_gptq_model()
+
+    def get_optimizer_with_param(self,
+                                 flattened_trainable_weights,
+                                 flattened_bias_weights,
+                                 trainable_quantization_parameters,
+                                 temperature_weights):
+        w2train = [*flattened_trainable_weights]
+        if self.gptq_config.is_gumbel:
+            if self.gptq_config.quantizer_config.temperature_learning:
+                w2train.extend(temperature_weights)
+
+        optimizer_with_param = [(self.gptq_config.optimizer, w2train)]
+        if self.gptq_config.train_bias or self.gptq_config.quantization_parameters_learning:
+            w2train_res = []
+            if self.gptq_config.train_bias:
+                if self.gptq_config.optimizer_bias is not None:
+                    optimizer_with_param.append((self.gptq_config.optimizer_bias, flattened_bias_weights))
+                else:
+                    w2train_res.extend(flattened_bias_weights)
+                    if self.gptq_config.optimizer_rest is None:
+                        Logger.error(
+                            "To enable bias micro training an additional optimizer is required, please define the optimizer_rest")
+            if self.gptq_config.quantization_parameters_learning:
+                if self.gptq_config.optimizer_quantization_parameter is not None:  # Ability to override optimizer
+                    optimizer_with_param.append((self.gptq_config.optimizer_quantization_parameter,
+                                                 trainable_quantization_parameters))
+                else:
+                    w2train_res.extend(trainable_quantization_parameters)
+                if self.gptq_config.optimizer_rest is None:
+                    Logger.error(
+                        "To enable bias micro training an additional optimizer is required, please define the optimizer_rest")
+            optimizer_with_param.append((self.gptq_config.optimizer_rest, w2train_res))
+
+        return optimizer_with_param
+
+
+    def compute_jacobian_based_weights(self,
+                                       representative_data_gen: Callable) -> np.ndarray:
+        """
+        Computes the jacobian-based weights using the framework's model_grad method per batch of images.
+
+        Args:
+            representative_data_gen: Dataset used for inference to compute the jacobian-based weights.
+
+        Returns: A vector of weights, one for each compare point,
+        to be used for the loss metric weighted average computation when running GPTQ training.
+        """
+        if self.gptq_config.use_jac_based_weights:
+            images = self._generate_images_batch(representative_data_gen, self.gptq_config.num_samples_for_loss)
+            points_apprx_jacobians_weights = []
+            for i in range(1, images.shape[0] + 1):
+                # Note that in GPTQ loss weights computation we assume that there aren't replacement output nodes,
+                # therefore, output_list is just the graph outputs, and we don't need the tuning factor for
+                # defining the output weights (since the output layer is not a compare point).
+                image_ip_gradients = self.fw_impl.model_grad(self.graph_float,
+                                                             {inode: images[i - 1:i] for inode in
+                                                              self.graph_float.get_inputs()},
+                                                             self.compare_points,
+                                                             output_list=[n.node for n in
+                                                                          self.graph_float.get_outputs()],
+                                                             all_outputs_indices=[],
+                                                             alpha=0,
+                                                             norm_weights=self.gptq_config.norm_weights)
+                points_apprx_jacobians_weights.append(image_ip_gradients)
+            return np.mean(points_apprx_jacobians_weights, axis=0)
+        else:
+            num_nodes = len(self.compare_points)
+            return np.asarray([1 / num_nodes for _ in range(num_nodes)])
+
+    @staticmethod
+    def _generate_images_batch(representative_data_gen: Callable, num_samples_for_loss: int) -> np.ndarray:
+        """
+        Construct batches of image samples for inference.
+
+        Args:
+            representative_data_gen: A callable method to retrieve images from Dataset.
+            num_samples_for_loss: Num of total images for evaluation.
+
+        Returns: A tensor of images batches
+        """
+        # First, select images to use for all measurements.
+        samples_count = 0  # Number of images we used so far to compute the distance matrix.
+        images = []
+        while samples_count < num_samples_for_loss:
+            # Get a batch of images to infer in both models.
+            inference_batch_input = representative_data_gen()
+            num_images = inference_batch_input[0].shape[0]
+
+            # If we sampled more images than we should,
+            # we take only a subset of these images and use only them.
+            if num_images > num_samples_for_loss - samples_count:
+                inference_batch_input = [x[:num_samples_for_loss - samples_count] for x in inference_batch_input]
+                assert num_samples_for_loss - samples_count == inference_batch_input[0].shape[0]
+                num_images = num_samples_for_loss - samples_count
+
+            images.append(inference_batch_input[0])
+            samples_count += num_images
+        return np.concatenate(images, axis=0)
 
     @abstractmethod
     def build_gptq_model(self):
