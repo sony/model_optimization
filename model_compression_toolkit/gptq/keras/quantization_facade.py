0e54053326bf1e5b3b3eb29d23480bb88e8028ae
Reuven <44209964+reuvenperetz@users.noreply.github.com>
2022-08-21 22:30:17 +0300

    Change defulat GPTQ config values (#347)
    * Change default GPTQ config values
    
    
    Co-authored-by: Reuven Peretz <reuvenp@altair-semi.com>

--- model_compression_toolkit/gptq/keras/quantization_facade.py
+++ model_compression_toolkit/gptq/keras/quantization_facade.py
@@ -31,14 +31,18 @@ from model_compression_toolkit.core.exporter import export_model
 from model_compression_toolkit.core.analyzer import analyzer_model_quantization
 from model_compression_toolkit.core.common.target_platform.targetplatform2framework import TargetPlatformCapabilities
 
-LR_DEFAULT = 0.2
+LR_DEFAULT = 0.05
+LR_REST_DEFAULT = 1e-4
+LR_BIAS_DEFAULT = 1e-4
+LR_QUANTIZATION_PARAM_DEFAULT = 1e-4
+
 if common.constants.FOUND_TF:
     import tensorflow as tf
     from model_compression_toolkit.core.keras.default_framework_info import DEFAULT_KERAS_INFO
     from model_compression_toolkit.core.keras.keras_implementation import KerasImplementation
     from model_compression_toolkit.core.keras.keras_model_validation import KerasModelValidation
     from tensorflow.keras.models import Model
-    from model_compression_toolkit.gptq.keras.gptq_loss import multiple_tensors_mse_loss
+    from model_compression_toolkit.gptq.keras.gptq_loss import GPTQMultipleTensorsLoss
     from keras.optimizer_v2.optimizer_v2 import OptimizerV2
     from model_compression_toolkit.core.keras.constants import DEFAULT_TP_MODEL
     from model_compression_toolkit.exporter import get_fully_quantized_keras_model
@@ -49,10 +53,9 @@ if common.constants.FOUND_TF:
 
     def get_keras_gptq_config(n_iter: int,
                               optimizer: OptimizerV2 = tf.keras.optimizers.Adam(learning_rate=LR_DEFAULT),
-                              optimizer_rest: OptimizerV2 = tf.keras.optimizers.Adam(),
-                              loss: Callable = multiple_tensors_mse_loss,
-                              log_function: Callable = None,
-                              train_bias: bool = True) -> GradientPTQConfig:
+                              optimizer_rest: OptimizerV2 = tf.keras.optimizers.Adam(learning_rate=LR_REST_DEFAULT),
+                              loss: Callable = GPTQMultipleTensorsLoss(),
+                              log_function: Callable = None) -> GradientPTQConfig:
         """
         Create a GradientPTQConfig instance for Keras models.
 
@@ -62,7 +65,6 @@ if common.constants.FOUND_TF:
             optimizer_rest (OptimizerV2): Keras optimizer to use for fine-tuning of the bias variable.
             loss (Callable): loss to use during fine-tuning. should accept 4 lists of tensors. 1st list of quantized tensors, the 2nd list is the float tensors, the 3rd is a list of quantized weights and the 4th is a list of float weights.
             log_function (Callable): Function to log information about the gptq process.
-            train_bias (bool): Whether to update the bias during the the fine-tuning or not.
 
         returns:
             a GradientPTQConfig object to use when fine-tuning the quantized model using gptq.
@@ -73,10 +75,6 @@ if common.constants.FOUND_TF:
 
             >>> gptq_conf = get_keras_gptq_config(n_iter=5)
 
-            To disable the biases training, one may set train_bias to false (enabled by default):
-
-            >>> gptq_conf = get_keras_gptq_config(n_iter=5, train_bias=false)
-
             Other Tensorflow optimizers can be passed:
 
             >>> gptq_conf = get_keras_gptq_config(n_iter=3, optimizer=tf.keras.optimizers.Nadam())
@@ -84,13 +82,16 @@ if common.constants.FOUND_TF:
             The configuration can be passed to :func:`~model_compression_toolkit.keras_post_training_quantization` in order to quantize a keras model using gptq.
 
         """
-
+        bias_optimizer = tf.keras.optimizers.SGD(learning_rate=LR_BIAS_DEFAULT)
+        optimizer_quantization_parameter = tf.keras.optimizers.Adam(learning_rate=LR_QUANTIZATION_PARAM_DEFAULT)
         return GradientPTQConfig(n_iter,
                                  optimizer,
                                  optimizer_rest=optimizer_rest,
                                  loss=loss,
                                  log_function=log_function,
-                                 train_bias=train_bias)
+                                 train_bias=True,
+                                 optimizer_bias=bias_optimizer,
+                                 optimizer_quantization_parameter=optimizer_quantization_parameter)
 
 
     def keras_gradient_post_training_quantization_experimental(in_model: Model,
