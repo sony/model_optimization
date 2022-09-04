(WIP) Adjust activation kpi computation to non-configurable nodes and fixed set_final_kpi

model_compression_toolkit/core/common/mixed_precision/kpi_tools/kpi_data.py
@@ -98,12 +98,13 @@ def compute_nodes_weights_params(graph: Graph, fw_info: FrameworkInfo) -> np.nda
 
     weights_params = []
     for n in graph.nodes:
-        node_num_weights_params = 0
-        for attr in fw_info.get_kernel_op_attributes(n.type):
-            if attr is not None:
-                node_num_weights_params += n.get_weights_by_keys(attr).flatten().shape[0]
+        if n.has_weights_quantization_enabled_candidate():
+            node_num_weights_params = 0
+            for attr in fw_info.get_kernel_op_attributes(n.type):
+                if attr is not None:
+                    node_num_weights_params += n.get_weights_by_keys(attr).flatten().shape[0]
 
-        weights_params.append(node_num_weights_params)
+            weights_params.append(node_num_weights_params)
 
     return np.array(weights_params)
 
@@ -121,10 +122,10 @@ def compute_activation_output_sizes(graph: Graph) -> np.ndarray:
 
     activation_outputs = []
     # Go over all nodes that have configurable activation.
-    # for n in graph.nodes:
-    for n in graph.get_sorted_activation_configurable_nodes():
-        node_output_size = n.get_total_output_params()
-        activation_outputs.append(node_output_size)
+    for n in graph.nodes:
+        if n.has_activation_quantization_enabled_candidate():
+            node_output_size = n.get_total_output_params()
+            activation_outputs.append(node_output_size)
 
     return np.array(activation_outputs)
 

model_compression_toolkit/core/runner.py
@@ -443,7 +443,10 @@ def _set_final_kpi(graph: Graph,
         else:
             non_conf_kpi = kpi_method([], graph, fw_info, fw_impl)
             conf_kpi = kpi_method(final_bit_widths_config, graph, fw_info, fw_impl)
-            final_kpis_dict[kpi_target] = kpi_aggr(np.concatenate([conf_kpi, non_conf_kpi]), False)[0]
+            if len(final_bit_widths_config) > 0:
+                final_kpis_dict[kpi_target] = kpi_aggr(np.concatenate([conf_kpi, non_conf_kpi]), False)[0]
+            else:
+                final_kpis_dict[kpi_target] = kpi_aggr(conf_kpi, False)[0]
 
     final_kpi = KPI()
     final_kpi.set_kpi_by_target(final_kpis_dict)