c1902803fd8a0ac9843b03fbd4801f4fa99d6c6c
elad cohen <elad.cohen@altair-semi.com>
2022-11-29 12:09:24 +0200

    remove test

--- tests/keras_tests/feature_networks_tests/test_features_runner.py
+++ tests/keras_tests/feature_networks_tests/test_features_runner.py
@@ -102,7 +102,6 @@ from tests.keras_tests.feature_networks_tests.feature_networks.network_editor.no
 from tests.keras_tests.feature_networks_tests.feature_networks.lut_quantizer import LUTWeightsQuantizerTest, \
     LUTActivationQuantizerTest
 from tests.keras_tests.feature_networks_tests.feature_networks.multi_head_attention_test import MultiHeadAttentionTest
-from tests.keras_tests.feature_networks_tests.feature_networks.layer_norm_substitution_test import LayerNormSub
 from tests.keras_tests.feature_networks_tests.feature_networks.qat.qat_test import QuantizationAwareTrainingTest, \
     QuantizationAwareTrainingQuantizersTest
 import tensorflow as tf
@@ -551,12 +550,6 @@ class FeatureNetworkTest(unittest.TestCase):
             MultiHeadAttentionTest(self, input_shapes,
                                    num_heads, qk_proj_dim, v_proj_dim, None,
                                    separate_key_value=separate_key_value, output_dim=14).run_test()
-
-    def test_layer_norm_substitution(self):
-        LayerNormSub(self, scale=True, center=True).run_test()
-        LayerNormSub(self, scale=True, center=False).run_test()
-        LayerNormSub(self, scale=False, center=True).run_test()
-        LayerNormSub(self, scale=False, center=False).run_test()
 
     def test_qat(self):
         QuantizationAwareTrainingTest(self, layers.Conv2D(3, 4, activation='relu')).run_test()
