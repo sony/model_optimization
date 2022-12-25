4fc457494ffbc6cc058525fb71362cabb18444a5
HVH <haihabi1986@gmail.com>
2022-12-25 11:53:12 +0200

    Bug fix in import for TF 2.8 and 2.9

--- model_compression_toolkit/qunatizers_infrastructure/keras/quantize_wrapper.py
+++ model_compression_toolkit/qunatizers_infrastructure/keras/quantize_wrapper.py
@@ -23,7 +23,8 @@ if FOUND_TF:
     import tensorflow as tf
     from tensorflow.python.util import tf_inspect
     from tensorflow_model_optimization.python.core.keras import utils
-    from keras.utils import deserialize_keras_object, serialize_keras_object
+
+    keras = tf.keras
 
     DISPATCHER = "dispatcher"
     LAYER = "layer"
@@ -75,7 +76,7 @@ if FOUND_TF:
 
             """
             base_config = super(KerasQuantizationWrapper, self).get_config()
-            config = {DISPATCHER: serialize_keras_object(self.dispatcher)}
+            config = {DISPATCHER: keras.utils.serialize_keras_object(self.dispatcher)}
             return dict(list(base_config.items()) + list(config.items()))
 
         @classmethod
@@ -90,7 +91,7 @@ if FOUND_TF:
             """
             config = config.copy()
 
-            dispatcher = deserialize_keras_object(
+            dispatcher = keras.utils.deserialize_keras_object(
                 config.pop(DISPATCHER),
                 module_objects=globals(),
                 custom_objects=None)
