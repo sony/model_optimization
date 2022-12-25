a449998329251af1352db57c6e12048e17b6b004
ariell <ariel.lapid@altair-semi.com>
2022-12-25 17:06:02 +0200

    Add tests for visualization coverage

--- tests/keras_tests/function_tests/test_tensorboard_writer.py
+++ tests/keras_tests/function_tests/test_tensorboard_writer.py
@@ -78,7 +78,7 @@ class TestLogger(unittest.TestCase):
         # ts = datetime.now(tz=None).strftime("%d%m%Y_%H%M%S")
         # common.Logger.set_log_file('/tmp/')
         # cls.addClassCleanup(shutil.rmtree, common.Logger.LOG_PATH)
-        mct.core.common.Logger.set_log_file('/tmp/')
+        mct.core.common.Logger.set_log_file('/tmp/1/')
         # model = MobileNet()
         # mct.keras_post_training_quantization(model, random_datagen, n_iter=1, analyze_similarity=True)
 
@@ -136,7 +136,7 @@ class MultipleOutputsTestLogger(unittest.TestCase):
         # ts = datetime.now(tz=None).strftime("%d%m%Y_%H%M%S")
         # common.Logger.set_log_file('/tmp/')
         # cls.addClassCleanup(shutil.rmtree, common.Logger.LOG_PATH)
-        mct.core.common.Logger.set_log_file('/tmp1/')
+        mct.core.common.Logger.set_log_file('/tmp/2/')
         # model = MobileNet()
         # mct.keras_post_training_quantization(model, random_datagen, n_iter=1, analyze_similarity=True)
 
