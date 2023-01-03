917d4527adf32b69110a28a6f887f23a881db6b4
eladc-git <eladcoh3@gmail.com>
2023-01-03 06:54:52 +0200

    fix after code review

--- model_compression_toolkit/qat/pytorch/quantizer/ste_rounding/symmetric_ste.py
+++ model_compression_toolkit/qat/pytorch/quantizer/ste_rounding/symmetric_ste.py
@@ -25,7 +25,7 @@ from model_compression_toolkit.qat.common.constants import FQ_MIN, FQ_MAX
 from model_compression_toolkit import qunatizers_infrastructure as qi
 from model_compression_toolkit.core.common import constants as C
 from model_compression_toolkit.core.pytorch.utils import to_torch_tensor
-from model_compression_toolkit.qunatizers_infrastructure.pytorch.quantizer_utils import ste_round, ste_clip, symmetric_quantizer
+from model_compression_toolkit.qat.pytorch.quantizer.quantizer_utils import ste_round, ste_clip, symmetric_quantizer
 
 
 class STEWeightQuantizer(qi.BasePytorchQuantizer):
