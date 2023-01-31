1773de334cb7dabd76247dd3cb7d93382e6bdc68
reuvenp <reuvenp@altair-semi.com>
2023-01-31 20:11:06 +0200

    fixes

--- model_compression_toolkit/quantizers_infrastructure/keras/inferable_quantizers/weights_inferable_quantizers/weights_symmetric_inferable_quantizer.py
+++ model_compression_toolkit/quantizers_infrastructure/keras/inferable_quantizers/weights_inferable_quantizers/weights_symmetric_inferable_quantizer.py
@@ -79,8 +79,6 @@ if FOUND_TF:
             Returns:
                 quantized tensor.
             """
-
-
             # If per-channel quantization is being used
             if self.per_channel:
                 # If a permutation vector has been created to move the channel axis to the last position
