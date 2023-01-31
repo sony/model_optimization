ca3f1ad238f090a967a16a45603465f59c8030b9
reuvenp <reuvenp@altair-semi.com>
2023-01-31 20:09:54 +0200

    fixes

--- model_compression_toolkit/quantizers_infrastructure/keras/inferable_quantizers/weights_inferable_quantizers/weights_pot_inferable_quantizer.py
+++ model_compression_toolkit/quantizers_infrastructure/keras/inferable_quantizers/weights_inferable_quantizers/weights_pot_inferable_quantizer.py
@@ -20,7 +20,6 @@ from model_compression_toolkit.quantizers_infrastructure.common.base_inferable_q
 
 if FOUND_TF:
     import tensorflow as tf
-    from tensorflow_model_optimization.python.core.quantization.keras import quant_ops
     from model_compression_toolkit.quantizers_infrastructure.keras.inferable_quantizers.base_pot_inferable_quantizer import BasePOTInferableQuantizer
 
     class WeightsPOTInferableQuantizer(BasePOTInferableQuantizer):
@@ -70,14 +69,6 @@ if FOUND_TF:
                 # set the permutation vector to None
                 self.perm_vec = None
 
-            # self.min_range = tf.Variable(self.min_range.flatten(), dtype=tf.float32)
-            # self.max_range = tf.Variable(self.max_range.flatten(), dtype=tf.float32)
-            # thresholds = np.arange(3)
-            #
-            # print(thresholds)
-            # self.min_range = -thresholds.flatten()
-            # self.max_range = (thresholds - thresholds / 128).flatten()
-
         def __call__(self, inputs: tf.Tensor):
             """
             Quantize the given inputs using the quantizer parameters.
@@ -95,20 +86,6 @@ if FOUND_TF:
                 if self.perm_vec:
                     # Transpose the input tensor to move the channel axis to the last position
                     inputs = tf.transpose(inputs, perm=self.perm_vec)
-
-                #
-                # q_tensor = quant_ops.LastValueQuantize(
-                #     inputs,
-                #     # tf.Tensor(self.min_range.flatten()),
-                #     # tf.Tensor(self.max_range.flatten()),
-                #     self.min_range,
-                #     self.max_range,
-                #     is_training=False,
-                #     num_bits=self.num_bits,
-                #     per_channel=self.per_channel,
-                #     symmetric=True,
-                #     narrow_range=False
-                # )
 
                 # Quantize the input tensor using per-channel quantization
                 q_tensor = tf.quantization.fake_quant_with_min_max_vars_per_channel(inputs,
