a103804d8ebf2593708f1ee435920064f1f17f40
Ofir Gordon <Ofir.Gordon@altair-semi.com>
2023-02-05 17:26:41 +0200

    Increase size of convolutions in tests to increase probability for some of them to change in gptq

--- tests/keras_tests/feature_networks_tests/feature_networks/gptq/gptq_test.py
+++ tests/keras_tests/feature_networks_tests/feature_networks/gptq/gptq_test.py
@@ -43,10 +43,10 @@ def build_model(in_input_shape: List[int]) -> keras.Model:
 
     """
     inputs = layers.Input(shape=in_input_shape)
-    x = layers.Conv2D(32, 4, bias_initializer='glorot_uniform')(inputs)
+    x = layers.Conv2D(64, 4, bias_initializer='glorot_uniform')(inputs)
     x = layers.BatchNormalization()(x)
     x = layers.PReLU()(x)
-    x = layers.Conv2D(32, 8, bias_initializer='glorot_uniform')(x)
+    x = layers.Conv2D(64, 8, bias_initializer='glorot_uniform')(x)
     x = layers.BatchNormalization()(x)
     outputs = layers.ReLU()(x)
     model = keras.Model(inputs=inputs, outputs=outputs)
