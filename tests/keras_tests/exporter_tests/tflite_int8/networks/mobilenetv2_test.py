a31aae84ad6378b53d02caac707dcdd64c864566
reuvenp <reuvenp@altair-semi.com>
2023-02-06 14:26:48 +0200

    Add exporter test with uniform activation quantization

--- tests/keras_tests/exporter_tests/tflite_int8/networks/mobilenetv2_test.py
+++ tests/keras_tests/exporter_tests/tflite_int8/networks/mobilenetv2_test.py
@@ -16,6 +16,9 @@
 from keras.applications import MobileNetV2
 import numpy as np
 
+from model_compression_toolkit.core.common.target_platform import QuantizationMethod
+from model_compression_toolkit.core.tpc_models.default_tpc.v5.tpc_keras import generate_keras_tpc
+from tests.common_tests.helpers.generate_test_tp_model import generate_test_tp_model
 from tests.keras_tests.exporter_tests.tflite_int8.tflite_int8_exporter_base_test import TFLiteINT8ExporterBaseTest
 import keras
 layers = keras.layers
@@ -33,4 +36,15 @@ class TestMBV2TFLiteINT8Exporter(TFLiteINT8ExporterBaseTest):
             assert 'quantization_parameters' in tensor.keys()
             scales = tensor['quantization_parameters']['scales']
             assert np.all(np.log2(scales) == np.round(np.log2(scales))), f'Expected all scales to be POT but scales are {scales} in tensor {tensor["name"]}'
+
+
+class TestMBV2UniformActivationTFLiteINT8Exporter(TestMBV2TFLiteINT8Exporter):
+
+    def get_tpc(self):
+        tp = generate_test_tp_model({'activation_quantization_method': QuantizationMethod.UNIFORM})
+        return generate_keras_tpc(name='uniform_conv2d_exporter', tp_model=tp)
+
+    def run_checks(self):
+        pass
+
 
