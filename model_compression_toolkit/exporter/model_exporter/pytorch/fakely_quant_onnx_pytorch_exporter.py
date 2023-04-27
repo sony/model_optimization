631d069f24182a5cb17525e593f57691c385492b
Ariel Lapid <57916763+lapid92@users.noreply.github.com>
2023-04-27 11:26:38 +0300

    Add low-bit onnx exporter (#639)
    Add low-bit onnx export
    Before exporting the model, iterate over the model layers and replace float weight with quantized values

--- model_compression_toolkit/exporter/model_exporter/pytorch/fakely_quant_onnx_pytorch_exporter.py
+++ model_compression_toolkit/exporter/model_exporter/pytorch/fakely_quant_onnx_pytorch_exporter.py
@@ -21,11 +21,15 @@ from model_compression_toolkit.core.pytorch.utils import to_torch_tensor
 from model_compression_toolkit.exporter.model_exporter.pytorch.base_pytorch_exporter import BasePyTorchExporter
 from packaging import version
 
+from model_compression_toolkit.quantizers_infrastructure import PytorchQuantizationWrapper
+from model_compression_toolkit.quantizers_infrastructure.inferable_infrastructure.common.constants import LAYER
+
 # ONNX opset version 16 is supported from PyTorch 1.12
 if version.parse(torch.__version__) < version.parse("1.12"):
     OPSET_VERSION = 15
 else:
     OPSET_VERSION = 16
+
 
 class FakelyQuantONNXPyTorchExporter(BasePyTorchExporter):
     """
@@ -69,6 +73,16 @@ class FakelyQuantONNXPyTorchExporter(BasePyTorchExporter):
         model_input = to_torch_tensor(next(self.repr_dataset())[0])
 
         Logger.info(f"Exporting PyTorch fake quant onnx model: {self.save_model_path}")
+
+        # Replace float weight with wrapped quantized weights
+        for layer in self.model.modules():
+            if isinstance(layer, PytorchQuantizationWrapper):
+                for name in layer.weights_quantizers.keys():
+                    quantized_weight = torch.nn.Parameter(layer.get_quantized_weights()[name]).detach()
+                    linear_layer = getattr(layer, LAYER)
+                    delattr(linear_layer, name)
+                    setattr(linear_layer, name, torch.nn.Parameter(quantized_weight))
+                layer.weights_quantizers = {}
 
         torch.onnx.export(self.model,
                           model_input,
