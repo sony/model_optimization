8abebb652983b66b6cec2bbd6d62fdaf26692f8a
Ofir Gordon <Ofir.Gordon@altair-semi.com>
2023-06-11 12:45:12 +0300

    Fix assertions

--- model_compression_toolkit/core/common/quantization/quantizers/uniform_quantizers.py
+++ model_compression_toolkit/core/common/quantization/quantizers/uniform_quantizers.py
@@ -24,10 +24,9 @@ from model_compression_toolkit.core.common.quantization.quantizers.quantizers_he
 def threshold_is_power_of_two(threshold: np.ndarray, per_channel: bool) -> bool:
     if per_channel:
         thresholds_per_channel = threshold.flatten()
-        return (thresholds_per_channel > 0).all() \
-               and (np.log2(thresholds_per_channel) == list(map(int, np.log2(thresholds_per_channel)))).all()
+        return (np.log2(thresholds_per_channel) == list(map(int, np.log2(thresholds_per_channel)))).all()
 
-    return threshold > 0 and np.log2(threshold) == int(np.log2(threshold))
+    return np.log2(threshold) == int(np.log2(threshold))
 
 
 def power_of_two_quantizer(tensor_data: np.ndarray,
@@ -56,6 +55,9 @@ def power_of_two_quantizer(tensor_data: np.ndarray,
         Logger.error(f"{THRESHOLD} parameter must be defined in 'quantization_params'")  # pragma: no cover
     if not threshold_is_power_of_two(threshold, per_channel):
         Logger.error(f"Expects {THRESHOLD} parameter to be a power of two, but got {threshold}")  # pragma: no cover
+    if (per_channel and (threshold <= 0).any()) or ((not per_channel) and threshold <= 0):
+        Logger.error(f"{THRESHOLD} parameter must positive")  # pragma: no cover
+
 
     return quantize_tensor(tensor_data,
                            threshold,
