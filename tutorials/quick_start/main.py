b4767c90e283e16de8e11ec409fb9de804bb6291
idanb <idanb@altair-semi.com>
2023-06-13 15:15:36 +0300

    Add license title to each file in quick_start

--- tutorials/quick_start/main.py
+++ tutorials/quick_start/main.py
@@ -1,3 +1,17 @@
+# Copyright 2023 Sony Semiconductor Israel, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 import argparse
 import importlib
 import logging
@@ -7,7 +21,6 @@ from common.results import write_results, read_models_list, parse_results, Quant
 from common.utils import find_modules
 from common.constants import MODEL_NAME, MODEL_LIBRARY, OUTPUT_RESULTS_FILE, TARGET_PLATFORM_NAME, \
     TARGET_PLATFORM_VERSION
-from model_compression_toolkit.core.common.user_info import UserInformation
 
 
 # Script to Evaluate and Compress Pre-trained Neural Network Model(s) using MCT (Model Compression Toolkit)
