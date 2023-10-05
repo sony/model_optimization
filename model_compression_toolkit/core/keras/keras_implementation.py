dbf3e9f0894a6b3af53e256d67613a4db1e0edcb
reuvenp <reuvenp@altair-semi.com>
2023-10-05 16:57:34 +0300

    remove model_grad file (keras only for now)

--- model_compression_toolkit/core/keras/keras_implementation.py
+++ model_compression_toolkit/core/keras/keras_implementation.py
@@ -31,8 +31,6 @@ from model_compression_toolkit.trainable_infrastructure.keras.quantize_wrapper i
 from model_compression_toolkit.core.common.mixed_precision.sensitivity_evaluation import SensitivityEvaluation
 from model_compression_toolkit.core.common.mixed_precision.set_layer_to_bitwidth import set_layer_to_bitwidth
 from model_compression_toolkit.core.common.similarity_analyzer import compute_kl_divergence, compute_cs, compute_mse
-from model_compression_toolkit.core.keras.back2framework.model_gradients import \
-    keras_iterative_approx_jacobian_trace
 from model_compression_toolkit.core.keras.constants import ACTIVATION, SOFTMAX, SIGMOID, ARGMAX, LAYER_NAME
 from model_compression_toolkit.core.keras.graph_substitutions.substitutions.batchnorm_reconstruction import \
     keras_batchnorm_reconstruction
