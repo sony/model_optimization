f5a3f99c4cb316e82984b2ae83c042f1c535f681
idanb <idanb@altair-semi.com>
2023-10-10 10:02:26 +0300

    Fixed few comments from PR

--- tutorials/resources/nanodet_keras_model.py
+++ tutorials/resources/nanodet_keras_model.py
@@ -1,7 +1,21 @@
+# Copyright 2023 Sony Semiconductor Israel, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 """
 Nanodet-Plus Object Detection Model
 
-This code defines a TensorFlow/Keras implementation of Nanodet-Plus object detection model, following
+This code contains a TensorFlow/Keras implementation of Nanodet-Plus object detection model, following
 https://github.com/RangiLyu/nanodet. This implementation also integrates the Nanodet-plus post-processing into the
 model.
 
