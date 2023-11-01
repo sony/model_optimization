a8a8ff7b8c11c291678d193ee6a4d6eaa702f6a5
reuvenp <reuvenp@altair-semi.com>
2023-11-01 11:17:12 +0200

    Add missing simd param in qat tutorial

--- tutorials/notebooks/example_pytorch_qat.py
+++ tutorials/notebooks/example_pytorch_qat.py
@@ -173,7 +173,8 @@ if __name__ == '__main__':
             quantization_preserving=False,
             fixed_scale=1.0,
             fixed_zero_point=0,
-            weights_multiplier_nbits=0)
+            weights_multiplier_nbits=0,
+            simd_size=None)
 
         default_configuration_options = tp.QuantizationConfigOptions([default_config])
         tp_model = tp.TargetPlatformModel(default_configuration_options)
