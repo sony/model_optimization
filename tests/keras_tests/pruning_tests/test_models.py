d7dc395fd368a1bee2b7ead89a4df9bfe2aa74a0
reuvenp <reuvenp@altair-semi.com>
2023-12-10 18:58:58 +0200

    Add licenses to new files

--- tests/keras_tests/pruning_tests/test_models.py
+++ tests/keras_tests/pruning_tests/test_models.py
@@ -1,3 +1,18 @@
+# Copyright 2023 Sony Semiconductor Israel, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
 import tempfile
 from enum import Enum
 
@@ -106,10 +121,10 @@ class ModelsPruningTest(unittest.TestCase):
         actual_cr = pruned_nparams / dense_nparams
         print(f"Target remaining cr: {cr*100}, Actual remaining cr: {actual_cr*100} ")
 
-        _, keras_file_path = tempfile.mkstemp('.keras')  # Path of exported model
-        print(f"Saving pruned model: {keras_file_path}")
-        keras.models.save_model(pruned_model, keras_file_path)
-
+        # _, keras_file_path = tempfile.mkstemp('.keras')  # Path of exported model
+        # print(f"Saving pruned model: {keras_file_path}")
+        # keras.models.save_model(pruned_model, keras_file_path)
+        #
 
 
         input_tensor = next(self.representative_dataset())[0]
