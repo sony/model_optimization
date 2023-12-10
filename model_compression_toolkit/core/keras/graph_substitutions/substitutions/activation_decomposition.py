7c8a7eb710ec1de4077d0a4b13a16da60a29ab83
elad cohen <elad.cohen@sony.com>
2023-12-10 15:15:31 +0200

    * Support CocoEm * Add CombinedNMS and SSDPostProcess from sony-custom-layers to tf_mp_tpc

--- model_compression_toolkit/core/keras/graph_substitutions/substitutions/activation_decomposition.py
+++ model_compression_toolkit/core/keras/graph_substitutions/substitutions/activation_decomposition.py
@@ -65,7 +65,7 @@ class ActivationDecomposition(common.BaseSubstitution):
 
         if ACTIVATION not in op2d_node.framework_attr:
             Logger.warning(f'Op2d node {op2d_node.name} of type {op2d_node.type} is missing an "{ACTIVATION}"'
-                           f' attribute -> Skipping substitution ActivationDecomposition')
+                           f' attribute -> Skipping substitution ActivationDecomposition')  # pragma: no cover
             return graph
 
         activation_node_name = op2d_node.name + '_post_activation'
