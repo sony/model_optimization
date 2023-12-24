19100b0e51e4a1c96addea1cfafe1b6e3455322e
reuvenp <reuvenp@altair-semi.com>
2023-12-24 15:37:17 +0200

    remove todos

--- model_compression_toolkit/core/common/mixed_precision/kpi_tools/__init__.py
+++ model_compression_toolkit/core/common/mixed_precision/kpi_tools/__init__.py
@@ -12,5 +12,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
-# TODO: this module kpi_tools should move out of mixed precision module as it uses other parts like pruning.
