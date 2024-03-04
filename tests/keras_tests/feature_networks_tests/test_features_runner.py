ff450c9907439b6c5787ae0387c86f683fa61412
reuvenp <reuvenp@altair-semi.com>
2024-03-04 16:46:27 +0200

    remove unneeded tests

--- tests/keras_tests/feature_networks_tests/test_features_runner.py
+++ tests/keras_tests/feature_networks_tests/test_features_runner.py
@@ -177,12 +177,6 @@ class FeatureNetworkTest(unittest.TestCase):
         LUTActivationQuantizerTest(self).run_test()
 
     def test_kmeans_quantizer(self):
-        # This test checks that the LUT- Kmeans quantization has a different result than symmetric uniform quantization
-        KmeansQuantizerTest(self, QuantizationMethod.LUT_POT_QUANTIZER).run_test()
-        KmeansQuantizerNotPerChannelTest(self, QuantizationMethod.LUT_POT_QUANTIZER).run_test()
-        KmeansQuantizerTest(self, QuantizationMethod.LUT_SYM_QUANTIZER).run_test()
-        KmeansQuantizerNotPerChannelTest(self, QuantizationMethod.LUT_SYM_QUANTIZER).run_test()
-
         # In this test we have weights with less unique values than the number of clusters
         KmeansQuantizerTestManyClasses(self, QuantizationMethod.LUT_POT_QUANTIZER,
                                        weights_n_bits=7).run_test()
