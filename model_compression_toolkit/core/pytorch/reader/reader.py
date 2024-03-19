a3dbd2c8d400395c1ceea5ab617f1955a69d7a43
elad-c <elad.cohen@sony.com>
2024-03-19 16:06:37 +0200

    Add error for torch.fx failure

--- model_compression_toolkit/core/pytorch/reader/reader.py
+++ model_compression_toolkit/core/pytorch/reader/reader.py
@@ -84,7 +84,12 @@ def fx_graph_module_generation(pytorch_model: torch.nn.Module,
         A fx.GraphModule (static model) representing the Pytorch model.
     """
     set_model(pytorch_model)
-    symbolic_traced = symbolic_trace(pytorch_model)
+
+    try:
+        symbolic_traced = symbolic_trace(pytorch_model)
+    except torch.fx.proxy.TraceError as e:
+        Logger.error(f'Error parsing model with torch.fx\n'
+                     f'fx error: {e}')
     inputs = next(representative_data_gen())
     input_for_shape_infer = [to_tensor(i) for i in inputs]
     ShapeProp(symbolic_traced).propagate(*input_for_shape_infer)
