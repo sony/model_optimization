Fix Hessian batch sample method to use remaining samples correctly (#1098)
In addition, fixed an issue with Hessian weights shape in mixed precision distance metric and some tests names.

---------

Co-authored-by: Ofir Gordon <Ofir.Gordon@altair-semi.com>

model_compression_toolkit/core/common/mixed_precision/sensitivity_evaluation.py
@@ -392,7 +392,11 @@ class SensitivityEvaluation:
             mean_distance_per_layer = ipts_distances.mean(axis=1)
 
             # Use weights such that every layer's distance is weighted differently (possibly).
-            mean_ipts_distance = np.average(mean_distance_per_layer, weights=metrics_weights_fn(ipts_distances))
+            weight_scores = metrics_weights_fn(ipts_distances)
+            weight_scores = np.asarray(weight_scores) if isinstance(weight_scores, List) else weight_scores
+            weight_scores = weight_scores.flatten()
+
+            mean_ipts_distance = np.average(mean_distance_per_layer, weights=weight_scores)
 
         mean_output_distance = 0
         if len(out_pts_distances) > 0: