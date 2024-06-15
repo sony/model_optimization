ddd178bc0b36530908c66d130f62e62101770469
reuvenp <reuvenp@altair-semi.com>
2024-06-15 21:45:30 +0300

    add similarity functions unit tests

--- tests/xquant_tests/pytorch_tests/test_model_analyzer.py
+++ tests/xquant_tests/pytorch_tests/test_model_analyzer.py
@@ -15,64 +15,72 @@
 from functools import partial
 
 import unittest
-from keras.layers import Input, Conv2D, Activation
-from keras.models import Model
+from model_compression_toolkit.core.pytorch.utils import get_working_device, to_torch_tensor
 
-from model_compression_toolkit.xquant.keras.model_analyzer import KerasModelAnalyzer
+from model_compression_toolkit.xquant.pytorch.model_analyzer import PytorchModelAnalyzer
+from tests.xquant_tests.pytorch_tests.test_xquant_report import random_data_gen
 import model_compression_toolkit as mct
-from tests.xquant_tests.keras_tests.test_xquant_report import random_data_gen
-
-
+from torch import nn
 class TestKerasModelAnalyzer(unittest.TestCase):
 
     def setUp(self):
+        self.device = get_working_device()
         self.float_model = self.get_model_to_test()
         self.repr_dataset = partial(random_data_gen, shape=self.get_input_shape())
-        self.quantized_model, _ = mct.ptq.keras_post_training_quantization(in_model=self.float_model,
-                                                                           representative_data_gen=self.repr_dataset)
+        self.quantized_model, _ = mct.ptq.pytorch_post_training_quantization(in_module=self.float_model,
+                                                                             representative_data_gen=self.repr_dataset)
 
-        self.analyzer = KerasModelAnalyzer()
+        self.float_model.to(self.device)
+        self.quantized_model.to(self.device)
+
+        self.analyzer = PytorchModelAnalyzer()
         self.float_name2quant_name = {
-            self.float_model.layers[1].name: self.quantized_model.layers[2].name
+            'conv': 'conv'
         }
 
     def get_input_shape(self):
-        return (8, 8, 3)
+        return (3, 8, 8)
 
     def get_model_to_test(self):
-        inputs = Input(shape=self.get_input_shape())
-        x = Conv2D(3, 3, name='conv2d')(inputs)
-        outputs = Activation('relu', name='activation')(x)
-        model = Model(inputs=inputs, outputs=outputs)
-        return model
+        class Model(nn.Module):
+            def __init__(self):
+                super(Model, self).__init__()
+                self.conv = nn.Conv2d(3, 3, 3, 1)
+                self.activation = nn.ReLU()
+
+            def forward(self, x):
+                x = self.conv(x)
+                x = self.activation(x)
+                return x
+
+        return Model()
 
     def test_extract_model_activations(self):
         float_activations, quant_activations = self.analyzer.extract_model_activations(
-            self.float_model, self.quantized_model, self.float_name2quant_name, next(self.repr_dataset())
+            self.float_model, self.quantized_model, self.float_name2quant_name, to_torch_tensor(next(self.repr_dataset()))
         )
         self.assertIsInstance(float_activations, dict)
         self.assertIsInstance(quant_activations, dict)
         self.assertEqual(len(float_activations), 2) # conv + output
         self.assertEqual(len(quant_activations), 2)  # conv + output
-        self.assertIn(self.float_model.layers[1].name, float_activations)
-        self.assertIn(self.quantized_model.layers[2].name, quant_activations)
+        self.assertIn('conv', float_activations)
+        self.assertIn('conv', quant_activations)
 
     def test_identify_quantized_compare_points(self):
         compare_points = self.analyzer.identify_quantized_compare_points(self.quantized_model)
         self.assertIsInstance(compare_points, list)
         self.assertEqual(len(compare_points), 1)
-        self.assertIn(self.quantized_model.layers[2].name, compare_points)
+        self.assertIn('conv', compare_points)
 
     def test_find_corresponding_float_layer(self):
-        corresponding_layer = self.analyzer.find_corresponding_float_layer(self.quantized_model.layers[2].name, self.quantized_model)
-        self.assertEqual(corresponding_layer, self.float_model.layers[1].name)
+        corresponding_layer = self.analyzer.find_corresponding_float_layer('conv', self.quantized_model)
+        self.assertEqual(corresponding_layer, 'conv')
 
     def test_extract_float_layer_names(self):
         layer_names = self.analyzer.extract_float_layer_names(self.float_model)
         self.assertIsInstance(layer_names, list)
-        self.assertIn('conv2d', layer_names)
+        self.assertIn('conv', layer_names)
         self.assertIn('activation', layer_names)
 
 if __name__ == '__main__':
     unittest.main()
-
