2fe0091aa5495b6975bb7ce790bb54931fac0ad5
Ariel Lapid <57916763+lapid92@users.noreply.github.com>
2024-08-04 13:26:30 +0300

    Update __init__.py

--- tutorials/notebooks/__init__.py
+++ tutorials/notebooks/__init__.py
@@ -12,3 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+TF_VER = 2.14.0
