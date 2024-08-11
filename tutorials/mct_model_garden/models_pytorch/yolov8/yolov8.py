5b0f0d62dd0ad761f9b19654af56343efd2c1102
idanb <idanb@altair-semi.com>
2024-08-11 17:53:46 +0300

    Update yolov8n tutorial with 16b manual configurations

--- tutorials/mct_model_garden/models_pytorch/yolov8/yolov8.py
+++ tutorials/mct_model_garden/models_pytorch/yolov8/yolov8.py
@@ -266,10 +266,10 @@ class Detect(nn.Module):
 
         # box decoding
         lt, rb = dfl.chunk(2, 1)
-        y1 = self.relu1(self.anchors.unsqueeze(0)[:, 0, :] - lt[:, 0, :])
-        x1 = self.relu2(self.anchors.unsqueeze(0)[:, 1, :] - lt[:, 1, :])
-        y2 = self.relu3(self.anchors.unsqueeze(0)[:, 0, :] + rb[:, 0, :])
-        x2 = self.relu4(self.anchors.unsqueeze(0)[:, 1, :] + rb[:, 1, :])
+        y1 = self.anchors.unsqueeze(0)[:, 0, :] - lt[:, 0, :]
+        x1 = self.anchors.unsqueeze(0)[:, 1, :] - lt[:, 1, :]
+        y2 = self.anchors.unsqueeze(0)[:, 0, :] + rb[:, 0, :]
+        x2 = self.anchors.unsqueeze(0)[:, 1, :] + rb[:, 1, :]
         y_bb = torch.stack((x1, y1, x2, y2), 1).transpose(1, 2)
         return y_bb, y_cls
 
