27b0742bc0a43365cf0e6e67f063caf5634fe1e0
Idan Ben Ami <109598548+Idan-BenAmi@users.noreply.github.com>
2024-08-12 11:38:34 +0300

    Set default shift negative in quantization config (#1157)
    * Enable shift negative by default in quantization config

--- tests/pytorch_tests/model_tests/feature_models/qat_test.py
+++ tests/pytorch_tests/model_tests/feature_models/qat_test.py
@@ -261,7 +261,7 @@ class QuantizationAwareTrainingMixedPrecisionCfgTest(QuantizationAwareTrainingTe
     def run_test(self):
         self._gen_fixed_input()
         model_float = self.create_networks()
-        config = mct.core.CoreConfig()
+        config = mct.core.CoreConfig(mct.core.QuantizationConfig(shift_negative_activation_correction=False))
         ru = mct.core.ResourceUtilization(np.inf, 47)  # inf memory
         qat_ready_model, quantization_info = mct.qat.pytorch_quantization_aware_training_init_experimental(model_float,
                                                                                                            self.representative_data_gen_experimental,
@@ -306,7 +306,7 @@ class QuantizationAwareTrainingMixedPrecisionRUCfgTest(QuantizationAwareTraining
     def run_test(self):
         self._gen_fixed_input()
         model_float = self.create_networks()
-        config = mct.core.CoreConfig()
+        config = mct.core.CoreConfig(mct.core.QuantizationConfig(shift_negative_activation_correction=False))
         ru = mct.core.ResourceUtilization(weights_memory=50, activation_memory=40)
         qat_ready_model, quantization_info = mct.qat.pytorch_quantization_aware_training_init_experimental(model_float,
                                                                                                            self.representative_data_gen_experimental,
