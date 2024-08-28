234d8dda15bfdef44ff774e6a31d5b17fe50fd45
Francesco Mattioli <Francesco.mttl@gmail.com>
2024-08-28 17:29:15 +0200

    fixed import

--- model_compression_toolkit/core/pytorch/__init__.py
+++ model_compression_toolkit/core/pytorch/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-from model_compression_toolkit.core.pytorch.pytorch_device_config import DeviceManager
+from model_compression_toolkit.core.pytorch.pytorch_device_config import set_working_device
