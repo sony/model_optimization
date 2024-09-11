fb349e9321fc7aa92e3071384bd03397afa5dd6c
elad-c <elad.cohen@sony.com>
2024-09-11 15:27:51 +0300

    Fix const quantization to "per-channel" and add relevant TPCs.

--- tests/pytorch_tests/model_tests/feature_models/activation_16bit_test.py
+++ tests/pytorch_tests/model_tests/feature_models/activation_16bit_test.py
@@ -60,6 +60,12 @@ class Activation16BitTest(BasePytorchFeatureNetworkTest):
         return Activation16BitNet()
 
     def compare(self, quantized_model, float_model, input_x=None, quantization_info=None):
+        x = torch.from_numpy(input_x[0].astype('float32'))
+        out_f = float_model(x)
+        quantized_model = quantized_model.to('cuda')
+        out_q = quantized_model(x.to('cuda'))
+        self.unit_test.assertTrue(out_f.shape == out_q.shape, "Output shape mismatch.")
+
         mul1_act_quant = quantized_model.mul_activation_holder_quantizer
         mul2_act_quant = quantized_model.mul_1_activation_holder_quantizer
         self.unit_test.assertTrue(mul1_act_quant.activation_holder_quantizer.num_bits == 16,
