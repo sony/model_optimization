89bd8d884a391a2a79500b35e223d9f18979442f
yardeny-sony <yardeny-sony@sony.com>
2024-09-25 19:33:10 +0300

    trigger

--- tests/pytorch_tests/model_tests/feature_models/scaled_dot_product_attention_test.py
+++ tests/pytorch_tests/model_tests/feature_models/scaled_dot_product_attention_test.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
 from tests.pytorch_tests.model_tests.base_pytorch_test import BasePytorchTest
 from torch import nn
 from packaging import version
