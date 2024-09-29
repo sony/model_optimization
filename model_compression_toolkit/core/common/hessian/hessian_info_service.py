fix + calc batch hessian on the fly

model_compression_toolkit/core/common/hessian/hessian_info_service.py
@@ -231,15 +231,16 @@ class HessianInfoService:
         return next_iter_remain_samples if next_iter_remain_samples is not None and len(next_iter_remain_samples) > 0 \
         and len(next_iter_remain_samples[0]) > 0 else None
 
-    def _compute_trackable_per_sample_hessian(self,
-                                              hessian_scores_request: HessianScoresRequest,
-                                              representative_dataset_gen) -> Dict[str, Dict['BaseNode', np.ndarray]]:
+    def compute_trackable_per_sample_hessian(self,
+                                             hessian_scores_request: HessianScoresRequest,
+                                             inputs_batch: List[np.ndarray]) -> Dict[str, Dict['BaseNode', np.ndarray]]:
         """
-        Compute hessian score per image hash.
+        Compute hessian score per image hash. We compute the score directly for images rather than via data generator,
+        as data generator might yield different images each time, depending on how it was defined,
 
         Args:
             hessian_scores_request: hessian scores request
-            representative_dataset_gen: representative dataset generator
+            inputs_batch: a list containing a batch of inputs.
 
         Returns:
             A dict of Hessian scores per image hash per layer {image hash: {layer: score}}
@@ -249,22 +250,22 @@ class HessianInfoService:
 
         hessian_score_by_image_hash = {}
 
-        for inputs_batch in representative_dataset_gen:
-            if not isinstance(inputs_batch, list):
-                raise TypeError('Expected representative data generator to yield a list of inputs')
-            if len(inputs_batch) > 1:
-                raise NotImplementedError('Per sample hessian computation is not supported for networks with multiple inputs')
-            # Get the framework-specific calculator Hessian-approximation scores
-            fw_hessian_calculator = self.fw_impl.get_hessian_scores_calculator(graph=self.graph,
-                                                                               input_images=inputs_batch,
-                                                                               hessian_scores_request=hessian_scores_request,
-                                                                               num_iterations_for_approximation=self.num_iterations_for_approximation)
-            hessian_scores = fw_hessian_calculator.compute()
-            for b in range(inputs_batch[0].shape[0]):
-                img_hash = self.calc_image_hash(inputs_batch[0][b])
-                hessian_score_by_image_hash[img_hash] = {
-                    node: score for node, score in zip(hessian_scores_request.target_nodes, hessian_scores)
-                }
+        if not isinstance(inputs_batch, list):
+            raise TypeError('Expected a list of inputs')
+        if len(inputs_batch) > 1:
+            raise NotImplementedError('Per-sample hessian computation is not supported for networks with multiple inputs')
+
+        # Get the framework-specific calculator Hessian-approximation scores
+        fw_hessian_calculator = self.fw_impl.get_hessian_scores_calculator(graph=self.graph,
+                                                                           input_images=inputs_batch,
+                                                                           hessian_scores_request=hessian_scores_request,
+                                                                           num_iterations_for_approximation=self.num_iterations_for_approximation)
+        hessian_scores = fw_hessian_calculator.compute()
+        for b in range(inputs_batch[0].shape[0]):
+            img_hash = self.calc_image_hash(inputs_batch[0][b])
+            hessian_score_by_image_hash[img_hash] = {
+                node: score[b] for node, score in zip(hessian_scores_request.target_nodes, hessian_scores)
+            }
 
         return hessian_score_by_image_hash
 

model_compression_toolkit/core/pytorch/hessian/activation_hessian_scores_calculator_pytorch.py
@@ -98,7 +98,7 @@ class ActivationHessianScoresCalculatorPytorch(HessianScoresCalculatorPytorch):
             return torch.randn(shape, device=device)
 
         if distribution == HessianEstimationDistribution.RADEMACHER:
-            return torch.where(torch.randint(0, 2, shape), 1, -1).to(device)
+            return torch.where(torch.randint(0, 2, shape, device=device).to(torch.bool), 1, -1).to(device)
 
         raise ValueError(f'Unknown distribution {distribution}')
 
@@ -177,8 +177,7 @@ class ActivationHessianScoresCalculatorPytorch(HessianScoresCalculatorPytorch):
                 # Computing the hessian-approximation scores by getting the gradient of (output * v)
                 hess_v = autograd.grad(outputs=f_v,
                                        inputs=ipt_tensor,
-                                       retain_graph=True,
-                                       allow_unused=True)[0]
+                                       retain_graph=True)[0]
 
                 hessian_approx_scores = hess_v ** 2
                 rank = len(hess_v.shape)
@@ -189,46 +188,3 @@ class ActivationHessianScoresCalculatorPytorch(HessianScoresCalculatorPytorch):
                 ipts_hessian_approx_scores[i] = (j * ipts_hessian_approx_scores[i] + hessian_approx_scores) / (j + 1)
 
         return ipts_hessian_approx_scores
-
-
-class SampleLayerHessianScoresCalculatorPytorch(ActivationHessianScoresCalculatorPytorch):
-
-    def compute(self) -> List[np.ndarray]:
-        """
-        Compute the scores that are based on the approximation of the Hessian w.r.t the requested target nodes' activations.
-
-        Returns:
-            List[np.ndarray]: List of approximated Hessian scores of shape (n_samples X n_channels)
-                for the requested nodes.
-        """
-        output, target_activation_tensors = self.forward_pass()
-        device = output.device
-
-        # Score is initialize to a scalar. Upon first update it will get the correct shape.
-        ipts_hessian_approx_scores = [torch.tensor(0., requires_grad=True, device=device)
-                                      for _ in range(len(target_activation_tensors))]
-
-        for j in tqdm(range(self.num_iterations_for_approximation),
-                      "Hessian random iterations"):  # Approximation iterations
-            v = torch.randint_like(output, high=2, device=device)
-            v[v == 0] = -1
-            out_v = torch.sum(v * output)
-            for i, ipt_tensor in enumerate(target_activation_tensors):  # Per Interest point activation tensor
-                # Computing the hessian-approximation scores by getting the gradient of (output * v)
-                hess_v = autograd.grad(outputs=out_v,
-                                       inputs=ipt_tensor,
-                                       retain_graph=True,
-                                       allow_unused=True)[0]
-
-                hessian_approx_scores = hess_v**2
-
-                rank = len(hess_v.shape)
-                if rank > 2:
-                    hessian_approx_scores = torch.mean(hess_v, dim=range(2, rank))
-
-                # Update node Hessian approximation mean over random iterations
-                ipts_hessian_approx_scores[i] = (j * ipts_hessian_approx_scores[i] + hessian_approx_scores) / (j + 1)
-
-        # Convert results to list of numpy arrays
-        hessian_results = [torch_tensor_to_numpy(h) for h in ipts_hessian_approx_scores]
-        return hessian_results

model_compression_toolkit/gptq/common/gptq_training.py
@@ -178,7 +178,7 @@ class GPTQTrainer(ABC):
             # If log normalization is not enabled, return the mean of the approximations across images
             return np.mean(hessian_approx_score_by_image, axis=0)
 
-    def _compute_sample_layer_attention_scores(self) -> Dict[str, Dict[BaseNode, np.ndarray]]:
+    def _compute_sample_layer_attention_scores(self, inputs_batch) -> Dict[str, Dict[BaseNode, np.ndarray]]:
         """
         Compute sample layer attention scores per image hash per layer.
 
@@ -186,10 +186,21 @@ class GPTQTrainer(ABC):
             A dictionary {img_hash: {layer: score}} where score is the
 
         """
-        hessian_score_per_image_per_layer = self._fetch_hessian_approximations(HessianScoresGranularity.PER_OUTPUT_CHANNEL)
+        request = self._build_hessian_request(HessianScoresGranularity.PER_OUTPUT_CHANNEL)
+        hessian_batch_size = self.gptq_config.hessian_weights_config.hessian_batch_size
+
+        hessian_score_per_image_per_layer = {}
+        # TODO Is it really needed if we compute on the fly per batch? Also if hessian batch is larger its ignored.
+        # If hessian batch is smaller than inputs batch, split it to hessian batches.
+        for i in range(0, inputs_batch[0].shape[0], hessian_batch_size):
+            inputs = [t[i: i+hessian_batch_size] for t in inputs_batch]
+            hessian_score_per_image_per_layer.update(
+                self.hessian_service.compute_trackable_per_sample_hessian(request, inputs)
+            )
+        # hessian_score_per_image_per_layer = self._fetch_hessian_approximations(HessianScoresGranularity.PER_OUTPUT_CHANNEL)
         for layers_score in hessian_score_per_image_per_layer.values():
             for k, t in layers_score.items():
-                layers_score[k] = t.max(axis=1)
+                layers_score[k] = t.max(axis=0)    # layer score is (channels,)
         return hessian_score_per_image_per_layer
 
     def _fetch_hessian_approximations(self, granularity: HessianScoresGranularity) -> Dict[BaseNode, List[List[float]]]:
@@ -199,12 +210,8 @@ class GPTQTrainer(ABC):
         Returns:
             Mapping of target nodes to their hessian approximations.
         """
-        hessian_scores_request = HessianScoresRequest(
-            mode=HessianMode.ACTIVATION,
-            granularity=granularity,
-            target_nodes=self.compare_points,
-            distribution=self.gptq_config.hessian_weights_config.estimator_distribution
-        )
+        hessian_scores_request = self._build_hessian_request(granularity)
+
         node_approximations = self.hessian_service.fetch_hessian(
             hessian_scores_request=hessian_scores_request,
             required_size=self.gptq_config.hessian_weights_config.hessians_num_samples,
@@ -212,6 +219,14 @@ class GPTQTrainer(ABC):
             per_sample_hash=self.gptq_config.hessian_weights_config.per_sample
         )
         return node_approximations
+
+    def _build_hessian_request(self, granularity):
+        return HessianScoresRequest(
+            mode=HessianMode.ACTIVATION,
+            granularity=granularity,
+            target_nodes=self.compare_points,
+            distribution=self.gptq_config.hessian_weights_config.estimator_distribution
+        )
 
     def _process_hessian_approximations(self, approximations: Dict[BaseNode, List[List[float]]]) -> List:
         """

model_compression_toolkit/gptq/pytorch/gptq_loss.py
@@ -88,10 +88,14 @@ def sample_layer_attention_loss(y_list: List[torch.Tensor],
     layers_mean_w = []
 
     for i, (y, x, w) in enumerate(zip(y_list, x_list, loss_weights)):
-        norm = (y - x).pow(2).sum(1)
+        # norm = (y - x).pow(2).sum(1)
+        norm = (y - x).pow(2).mean(1)
         if len(norm.shape) > 1:
             norm = norm.flatten(1).mean(1)
         loss += torch.mean(w * norm)
         layers_mean_w.append(w.mean())
+
+    # loss = loss / len(x_list)
     loss = loss / torch.stack(layers_mean_w).max()
     return loss
+

model_compression_toolkit/gptq/pytorch/gptq_training.py
@@ -111,8 +111,10 @@ class PytorchGPTQTrainer(GPTQTrainer):
         if hessian_cfg.per_sample:
             assert (hessian_cfg.norm_scores is False and hessian_cfg.log_norm is False and
                     hessian_cfg.scale_log_norm is False), hessian_cfg
-            # self.hessian_score_per_image_per_layer = self._fetch_hessian_approximations()
-            self.hessian_score_per_image_per_layer = self._compute_sample_layer_attention_scores()
+            # TODO if a representative dataset is fixed (same images in each epoch) we can precalculate.
+            # However if images differ between epochs, we have to calculate their hessians each time and pre-calculation
+            # will be a waste. Currently it is calculated on-demand during the training loop.
+            self.hessian_score_per_image_per_layer = {}
         else:
             self.weights_for_average_loss = to_torch_tensor(self.compute_hessian_based_weights())
 
@@ -271,11 +273,10 @@ class PytorchGPTQTrainer(GPTQTrainer):
             for _ in epochs_pbar:
                 with tqdm(data_function(), position=1, leave=False) as data_pbar:
                     for data in data_pbar:
+                        weights = to_torch_tensor(self._get_samples_weights_for_loss(data))
                         input_data = [d * self.input_scale for d in data]
                         input_tensor = to_torch_tensor(input_data)
                         y_float = self.float_model(input_tensor)  # running float model
-                        # weights are either (layers,) or (batch X layers X channels)
-                        weights = to_torch_tensor(self._get_samples_weights_for_loss(input_tensor))
                         loss_value, grads = self.compute_gradients(y_float, input_tensor, weights)
                         # Run one step of gradient descent by updating the value of the variables to minimize the loss.
                         for (optimizer, _) in self.optimizer_with_param:
@@ -290,21 +291,25 @@ class PytorchGPTQTrainer(GPTQTrainer):
 
     # TODO move to common after ctor refactor
     def _get_samples_weights_for_loss(self, input_tensors: List[torch.Tensor]):
-        if self.hessian_score_per_image_per_layer is None:
-            assert self.weights_for_average_loss is not None
+        if self.weights_for_average_loss is not None:
+            assert self.hessian_score_per_image_per_layer is None
             return self.weights_for_average_loss
 
+        # assert self.hessian_score_per_image_per_layer
         if len(input_tensors) > 1:
             raise NotImplementedError('Sample-Layer attention is not currently supported for networks with multiple inputs')
 
         scores = []
-        batch = input_tensors[0].detach().cpu().numpy()
+        batch = input_tensors[0]
         img_hashes = [self.hessian_service.calc_image_hash(img) for img in batch]
         for img_hash in img_hashes:
+            if img_hash not in self.hessian_score_per_image_per_layer:
+                score_per_image_layer_per = self._compute_sample_layer_attention_scores(input_tensors)
+                self.hessian_score_per_image_per_layer.update(score_per_image_layer_per)
             img_scores_per_layer: Dict[BaseNode, np.ndarray] = self.hessian_score_per_image_per_layer[img_hash]
             img_scores = np.stack(list(img_scores_per_layer.values()), axis=0)
             scores.append(img_scores)
-        return np.stack(scores, axis=0)
+        return np.stack(scores, axis=1)    # layers X images
 
     def update_graph(self) -> Graph:
         """